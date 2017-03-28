a918d2a3c868c34ce732d4629436399f1b3b692d
Craig Swank <craigswank@gmail.com>
2017-03-27 19:09:49 -0600

    moved page struct to own file

--- internal/views/views.go
+++ internal/views/views.go
@@ -30,56 +30,6 @@ type row struct {
 	data string
 }
 
-type pages struct {
-	p []page
-}
-
-func (p *pages) header() string {
-	l := len(p.p)
-	return p.p[l-1].header
-}
-
-func (p *pages) cursor() int {
-	l := len(p.p)
-	return p.p[l-1].cursor
-}
-
-func (p *pages) body(page int) []row {
-	l := len(p.p)
-	return p.p[l-1].body[page]
-}
-
-func (p *pages) sel(cur int) (page, row) {
-	l := len(p.p)
-	page := p.p[l-1]
-	page.cursor = cur
-	p.p[l-1] = page
-	return page, page.body[page.page][cur]
-}
-
-func (p *pages) pop() {
-	l := len(p.p)
-	if l == 1 {
-		return
-	}
-	p.p = p.p[:l-1]
-}
-
-func (p *pages) add(n page) {
-	p.p = append(p.p, n)
-}
-
-type page struct {
-	page   int
-	header string
-	body   [][]row
-	cursor int
-
-	next    func(int, string) (page, error)
-	forward func(int, string) ([]row, error)
-	back    func(int, string) ([]row, error)
-}
-
 type View interface {
 	Render(g *ui.Gui, v *ui.View) error
 }
