dd90795d290d138b3e1f3b72fb2a5e63cfb49b5d
Craig Swank <craigswank@gmail.com>
2017-07-24 06:12:39 -0600

    wip

--- internal/views/movies.go
+++ internal/views/movies.go
@@ -17,6 +17,39 @@ var (
 )
 
 func getJumpSteps() []helpStep {
+	partitionHeader := c1("offset       message    topic: items partition: 0 start: 0 end: 1017")
+	partitionRows := c2(`0            Robert,Scott,46
+1            Richard,Scott,58
+2            Martha,Payne,13
+3            Tabitha,Bass,71
+4            Katherine,Robertson,83
+5            Jane,Hawkins,82
+6            Bob,Nash,5
+7            Jacqueline,Scott,61
+8            Dave,Pratt,37
+9            Debra,Gross,63
+10           Ashley,Gross,19`)
+
+	partitionTpl := fmt.Sprintf(`%s
+%s
+%%s`, partitionHeader, partitionRows)
+
+	partitionRows2 := c2(`25           Yolanda,Crawford,66
+26           Donnie,Lopez,77
+27           Grace,Bass,67
+28           Abraham,Pratt,9
+29           Bobbie,Cooper,56
+30           Sabrina,Nash,47
+31           Elias,Singleton,47
+32           Sylvester,Cooper,16
+33           Katherine,Scott,42
+34           Bobbie,Steele,43
+35           Donnie,Pratt,67`)
+
+	partitionTpl2 := fmt.Sprintf(`%s
+%s
+%%s`, partitionHeader, partitionRows2)
+
 	header := c1("partition     1st offset             current offset         last offset            size")
 	rows := c2(`0             0                      %s                   1100                   1100
 1             55                     %s                   2405                   2320`)
@@ -34,6 +67,42 @@ func getJumpSteps() []helpStep {
 %%s`, header, rows)
 
 	return []helpStep{
+		{
+			caption:  "when jumping on a partition, the number you",
+			template: partitionTpl,
+			args:     []interface{}{""},
+			duration: 500 * time.Millisecond,
+		},
+		{
+			caption:  "when jumping on a partition, the number you",
+			template: partitionTpl,
+			args:     []interface{}{c1("jump: ")},
+			duration: 500 * time.Millisecond,
+		},
+		{
+			caption:  "when jumping on a partition, the number you",
+			template: partitionTpl,
+			args:     []interface{}{c1("jump: 2")},
+			duration: 500 * time.Millisecond,
+		},
+		{
+			caption:  "when jumping on a partition, the number you",
+			template: partitionTpl,
+			args:     []interface{}{c1("jump: 25")},
+			duration: 500 * time.Millisecond,
+		},
+		{
+			caption:  "enter sets the current offset",
+			template: partitionTpl,
+			args:     []interface{}{c1("jump: 25")},
+			duration: 500 * time.Millisecond,
+		},
+		{
+			caption:  "enter sets the current offset",
+			template: partitionTpl2,
+			args:     []interface{}{""},
+			duration: 4000 * time.Millisecond,
+		},
 		{
 			caption:  "when you jump on a topic the number you enter",
 			template: tpl,
